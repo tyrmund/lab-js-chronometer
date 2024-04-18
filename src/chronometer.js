80045287c530f5b73f45bb3d6a3986cdb480ba76
Rodolfo Bittkau <tyrmund@gmail.com>
2024-04-18 19:33:40 +0200

    last one remaining

--- src/chronometer.js
+++ src/chronometer.js
@@ -1,33 +1,58 @@
 class Chronometer {
   constructor() {
     // ... your code goes here
+    this.currentTime = 0,
+      this.intervalId = null
   }
 
   start(printTimeCallback) {
     // ... your code goes here
+    this.intervalId = setInterval(() => {
+      this.currentTime++
+      if (printTimeCallback) {
+        printTimeCallback()
+      }
+    }, 1000)
   }
 
   getMinutes() {
     // ... your code goes here
+    const numberOfMinutes = Math.floor(this.currentTime / 60)
+    return numberOfMinutes
   }
 
   getSeconds() {
     // ... your code goes here
+    const currentSeconds = this.currentTime % 60
+    return currentSeconds
   }
 
   computeTwoDigitNumber(value) {
     // ... your code goes here
+    const toString = String(value)
+    if (toString.length === 1) return toString.padStart(2, '0')
+    else return toString
   }
 
   stop() {
     // ... your code goes here
+    clearInterval(this.intervalId)
   }
 
   reset() {
     // ... your code goes here
+    this.currentTime = 0
   }
 
   split() {
     // ... your code goes here
+    const minutes = this.getMinutes()
+    const formattedMinutes = this.computeTwoDigitNumber(minutes)
+
+    const seconds = this.getSeconds()
+    const formattedSeconds = this.computeTwoDigitNumber(seconds)
+
+    const formattedTime = `${formattedMinutes}:${formattedSeconds}`
+    return formattedTime
   }
 }
